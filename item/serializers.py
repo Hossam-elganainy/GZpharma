238b56caf44564ff069275837b51377a52ac6e0d
Hossam-elganainy <hanyhossam93@gmail.com>
2025-02-23 02:28:40 +0200

    new

--- item/serializers.py
+++ item/serializers.py
@@ -5,3 +5,8 @@ class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
         fields = '__all__'
+
+    def create(self, validated_data):
+        if isinstance(validated_data, list):
+            return Item.objects.bulk_create([Item(**item) for item in validated_data])
+        return super().create(validated_data)
--- item/views.py
+++ item/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Item
 from .serializers import ItemSerializer
-from rest_framework import generics
-from django_filters.rest_framework import DjangoFilterBackend
 from .filters import ItemFilter
-from rest_framework.permissions import IsAuthenticated
+
 class ItemList(generics.ListCreateAPIView):
     permission_classes = [IsAuthenticated]
     queryset = Item.objects.all()
@@ -12,7 +13,10 @@ class ItemList(generics.ListCreateAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_class = ItemFilter
 
-
-
-
-
+    def create(self, request, *args, **kwargs):
+        is_bulk = isinstance(request.data, list)  # Check if data is a list
+        serializer = self.get_serializer(data=request.data, many=is_bulk)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
